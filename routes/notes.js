045598029731b0012e40a0deac887fcc472ec37b
dMosca <devinmosca@yahoo.com>
2021-09-07 16:19:51 -0600

    Routes added

---
+++ routes/notes.js
@@ -0,0 +1,63 @@
+const note = require("express").Router();
+const uuid = require("../helpers/uuid");
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+  } = require('../helpers/fsUtils');
+
+// GET Route for all notes
+note.get("/api/notes", (req, res) => 
+    readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)))
+);
+
+// GET Route for a specific note
+note.get('/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        const result = json.filter((note) => note.id === noteId);
+        return result.length > 0
+          ? res.json(result)
+          : res.json("No note with that ID");
+      });
+  });
+
+// POST Route for submitting new note
+note.post("/", (req, res) => {
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+    // If all the required properties are present
+    if (req.body) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+        id: uuid(),
+      };
+      readAndAppend(newNote, "./db/db.json");
+      res.json("Note Added!");
+    } else {
+      res.error("Note did NOT add!!");
+    }
+  });
+
+// DELETE Route for a specific tip
+note.delete("/:id", (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        // Make a new array of all notes except the one with the ID provided in the URL
+        const result = json.filter((note) => note.id !== noteId);
+  
+        // Save that array to the filesystem
+        writeToFile('./db/db.json', result);
+  
+        // Respond to the DELETE request
+        res.json(`Item ${noteId} has been deleted 🗑️`);
+      });
+});
+
+  module.exports = fb;
--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const path = require("path");
-const fs = require("fs");
-const uuid = require("./helpers/uuid");
 const api = require('./routes/index.js');
 
 const PORT = process.env.port || 3001;
@@ -24,81 +22,10 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// GET Route for /api/notes
-app.get("/api/notes", (req, res) => {
-    res.json(db)
-});
-
 // GET Route for catch all
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
 );
-
-/**
- *  Function to write data to the JSON file given a destination and some content
- *  @param {string} destination The file you want to write to.
- *  @param {object} content The content you want to write to the file.
- *  @returns {void} Nothing
- */
-const writeToFile = (destination, content) =>
-  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-    err ? console.error(err) : console.info(`\nData written to ${destination}`)
-  );
-/**
- *  Function to read data from a given a file and append some content
- *  @param {object} content The content you want to append to the file.
- *  @param {string} file The path to the file you want to save to.
- *  @returns {void} Nothing
- */
-const readAndAppend = (content, file) => {
-  fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      const parsedData = JSON.parse(data);
-      parsedData.push(content);
-      writeToFile(file, parsedData);
-    }
-  });
-};
-
-// POST Route for new note
-app.post("/api/notes", (req, res) => {
-    const { title, text } = req.body;
-
-    if (req.body) {
-        const newNote = {
-            title,
-            text,
-            id: uuid(),
-        };
-
-        readAndAppend(newNote, "./db/db.json");
-        res.json("Note Added!");
-    } else {
-        res.error("Note did NOT add!!");
-    }
-});
-
-// DELETE Route for deleting note
-app.delete("/api/notes/:id", (req, res) => {
-    const deleteNote = req.params.id;
-    console.log(`Deleting note: ${deleteNote}🗑️`);
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
-        if(err) {
-            console.error(err);
-        } else {
-            const parsedData = JSON.parse(data);
-            parsedData.forEach(element, i => {
-                if (selectedNote === element.id) {
-                    parsedData.splice(i, 1)
-                }
-            });
-            writeToFile("./db/db.json", parsedData);
-            res.json("Note Deleted!");
-        };
-    });
-});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
